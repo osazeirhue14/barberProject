7ff36588cb47c1ae48332238fb9caa961209df57
Osaze <osaze.irhue.2022@mumail.ie>
2025-09-14 15:26:58 +0100

    Added Firebase review section and updated Home.js

--- my-barber-app/src/pages/Booking.js
+++ my-barber-app/src/pages/Booking.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./booking.css";
+import "./Booking.css";
 
 function Booking() {
   const [total, setTotal] = useState(0);
